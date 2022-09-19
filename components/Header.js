68536b28d448b94fe40726fa07432b125cc1f06f
Luqman Arshad <luqmanarshad49@gmail.com>
2022-09-19 19:37:54 +0500

    2nd commit

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
-import { MagnifyingGlassIcon,GlobeAltIcon,Bars3Icon,UserCircleIcon } from '@heroicons/react/24/solid'
+import { MagnifyingGlassIcon,GlobeAltIcon,Bars3Icon,UserCircleIcon,UsersIcon } from '@heroicons/react/24/solid'
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { useRouter } from 'next/router';
 
-export default function Header() {
+
+
+export default function Header({placeholder}) {
+    const [searchInput, setSearchInput] = useState('')
+    const [startDate,setStartDate] = useState(new Date())
+    const [endDate,setEndDate] = useState(new Date())
+    const [numberOfGuests, setNumberOfGuests] = useState(1)
+    const router = useRouter();
+    
+    const selectionRange ={
+        startDate:startDate,
+        endDate:endDate,
+        key:'selection'
+    }
+
+    const handleSelect = (ranges)=>{
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+
+    const resetInput = ()=>{
+        setSearchInput('')
+    }
+
+    const search = ()=>{
+       router.push({pathname:'/search',query:{
+        location:searchInput,
+        startDate:startDate.toISOString(),
+        endDate:endDate.toISOString(),
+        numOfGuests:numberOfGuests
+    }})
+    }
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-1 px-1 md:px-10'>
         {/* left */}
-        <div className='relative flex items-center h-10  my-auto cursor-pointer '>
+        <div onClick={()=>router.push('/')} className='relative flex items-center h-10  my-auto cursor-pointer '>
             <Image
             src="/logo.png" 
             layout='fill'
@@ -16,7 +51,7 @@ export default function Header() {
         </div>
         {/* middle */}
         <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-            <input type='text' placeholder='search' className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-300' />
+            <input value={searchInput} onChange={(e)=>setSearchInput(e.target.value)} type='text' placeholder={placeholder || 'Start Your Search'} className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-300' />
             <MagnifyingGlassIcon  className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2'/>
         </div>
         {/* right */}
@@ -28,6 +63,31 @@ export default function Header() {
                 <UserCircleIcon className='h-6'/>
             </div>
         </div>
+
+        {searchInput && (
+            <div className='flex flex-col col-span-3 mx-auto '>
+                <DateRangePicker 
+                    ranges={[selectionRange]} 
+                    minDate={new Date()} 
+                    rangeColors={['#FD5B61']}
+                    onChange={handleSelect}
+                />
+                <div className='flex items-center border-b mb-5'>
+                    <h2 className='text-2xl flex-grow font-semibold'>Number of Guests</h2>
+                    <UsersIcon className='h-5' />
+                    <input 
+                        type='number' 
+                        min={1} 
+                        value={numberOfGuests} 
+                        onChange={(e)=>setNumberOfGuests(e.target.value)} className='w-12 pl-2 text-lg outline-none text-red-400'
+                    />
+                </div>
+                <div className='flex mb-5'>
+                        <button className='flex-grow text-gray-400' onClick={resetInput} >Cancel</button>
+                        <button className='flex-grow text-red-400' onClick={search}>Search</button>
+                    </div>
+            </div>
+        )}
     </header>
   )
 }
